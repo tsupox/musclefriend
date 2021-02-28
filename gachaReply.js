2f60b29a1ca8a1eef4fc9c435ddfb41c0cd9e735
tsupox <tsupox@gmail.com>
2021-02-28 22:57:30 +0900

    v0.6.0 gachaReply

---
+++ gachaReply.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+const moment = require("moment");
+const randomReply = require('./randomReply.js');
+const dataFile = './data/gachaReply.json';
+
+let gachaReply = {
+    database: null,
+
+    init: () => {
+        gachaReply.database = JSON.parse(fs.readFileSync(dataFile, "utf8"));
+    },
+
+    getList: () => {
+        return gachaReply.database;
+    },
+
+    getReply: () => {
+        return randomReply.getRandom(gachaReply.database);
+    },
+
+    addCommand: (args) => {
+        //insert
+        console.log('add gacha: ' + args.join(' '))
+
+        //ランダム返信
+        gachaReply.database.push(args.join(' '));
+        //file write
+        fs.writeFileSync(dataFile, JSON.stringify(gachaReply.database));
+        return true;
+    },
+
+    deleteCommand: (args) => {
+        //delete
+        let exist = false;
+        console.log('delete command: ' + args.join(' '))
+
+        gachaReply.database.forEach((c, i) => {
+            if (args.join(" ").match(new RegExp("^" + c + "$"))) {
+                gachaReply.database.splice(i, 1)
+                exist = true;
+            }
+        });
+        if (exist) fs.writeFileSync(dataFile, JSON.stringify(gachaReply.database));
+        return exist
+    },
+
+    backupJson: () => {
+        let now = moment();
+        fs.writeFileSync(dataFile + "_" + now.format("YYYYMMDDHHmm"), JSON.stringify(gachaReply.database))
+        //TODO: S3 バックアップ
+    },
+
+}
+
+module.exports = gachaReply
---
+++ test/gachaReply.test.js
@@ -0,0 +1,76 @@
+const fs = require('fs')
+const expect = require('chai').expect
+const mockedEnv = require('mocked-env')
+const mockDate = require('mockdate');
+const gachaReply = require('../gachaReply.js');
+const dataFile = './data/gachaReply.json';
+
+// set mock env
+// let restore = mockedEnv({
+//     "DAY_START_TIME": "5"
+// })
+
+const setDatabase = () => {
+    gachaReply.database = [
+        "テスト1", "テスト2", "テスト3"
+    ]
+}
+
+describe('gachaReply.js', () => {
+    describe('getList()', () => {
+        it('登録ガチャ言葉一覧取得', () => {
+            setDatabase()
+            let result = gachaReply.getList()
+            expect(result).to.have.deep.equals(["テスト1", "テスト2", "テスト3"])
+        });
+    });
+    describe('getReply()', () => {
+        it('登録済みのガチャ言葉', () => {
+            setDatabase()
+            let result = gachaReply.getReply()
+            expect(result).to.be.oneOf(["テスト1", "テスト2", "テスト3"])
+        });
+    });
+    describe('addCommand()', () => {
+        it('ガチャ言葉登録 - 新規登録', () => {
+            setDatabase()
+            let gachaLength = gachaReply.database.length
+
+            let result = gachaReply.addCommand(["テストテスト", "テストー"])
+            // assert result
+            expect(result).to.be.true
+            // assert variable
+            expect(gachaReply.database).to.have.lengthOf(gachaLength + 1)
+            expect(gachaReply.database[gachaLength]).to.deep.equal("テストテスト テストー")
+            // assert file data
+            let resultFile = JSON.parse(fs.readFileSync(dataFile, "utf8"));
+            expect(resultFile).to.deep.equals(gachaReply.database)
+        });
+    });
+    describe('deleteCommand()', () => {
+        it('ガチャ言葉削除 - 言葉が存在する場合', () => {
+            setDatabase()
+            let gachaLength = gachaReply.database.length
+
+            let result = gachaReply.deleteCommand(["テスト2"])
+            // assert result
+            expect(result).to.be.true
+            // assert variable
+            expect(gachaReply.database).to.have.lengthOf(gachaLength - 1)
+            expect(gachaReply.database[1]).to.not.deep.equal("テスト2")
+            // assert file data
+            let resultFile = JSON.parse(fs.readFileSync(dataFile, "utf8"));
+            expect(resultFile).to.deep.equals(gachaReply.database)
+        });
+        it('ガチャ言葉削除 - 言葉が存在しない場合', () => {
+            setDatabase()
+            let gachaLength = gachaReply.database.length
+
+            let result = gachaReply.deleteCommand(["存在しない言葉"])
+            // assert result
+            expect(result).to.be.false
+            // assert variable
+            expect(gachaReply.database).to.have.lengthOf(gachaLength)
+        });
+    });
+});
